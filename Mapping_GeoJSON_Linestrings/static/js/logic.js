44cc03400cbfcae2142657ed1de958597bdcd675
dangzzmedina <daniel_medina92@hotmail.com>
2021-08-24 00:46:02 -0500

    changes polygon

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -26,10 +26,10 @@ let map = L.map('mapid', {
 })
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
-// Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/dangzzmedina/Mapping_Earthquakes/main/majorAirports.json";
+// Accessing the Toronto airline routes GeoJSON URL.
+let torontoData = "https://raw.githubusercontent.com/dangzzmedina/Mapping_Earthquakes/main/torontoRoutes.json";
 // Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
+d3.json(torontoData).then(function(data) {
   console.log(data);
 // Creating a GeoJSON layer with the retrieved data.
 L.geoJson(data).addTo(map);
